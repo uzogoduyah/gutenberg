578fb90fcd6fa9324047e9181dd0ef873031068a
Gerardo <gerardo.pacheco@automattic.com>
2022-09-23 16:37:12 +0200

    Mobile - Update React native gesture handler

--- packages/react-native-editor/android/app/src/main/java/com/gutenberg/MainActivity.java
+++ packages/react-native-editor/android/app/src/main/java/com/gutenberg/MainActivity.java
@@ -6,9 +6,6 @@ import androidx.annotation.Nullable;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
 import org.wordpress.mobile.WPAndroidGlue.GutenbergProps;
 
 import java.util.Locale;
@@ -27,11 +24,6 @@ public class MainActivity extends ReactActivity {
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-
             @Nullable
             @Override
             protected Bundle getLaunchOptions() {
